561327d27dfbfad83bd11b201901af3d0175bd02
W.J. Llope <wjllope@wayne.edu>
2026-03-11 15:22:47 -0400

    initial version

---
+++ ddDIRCactionStep.cxx
@@ -0,0 +1,6 @@
+#include "DDG4/Factories.h"
+
+#include "ddDIRCactionStep.h"
+
+
+DECLARE_GEANT4ACTION(ddDIRCactionStep) //The argument here is what replaces something like "Geant4SteppingAction/blahblah" in the syntax to use this in npsim
